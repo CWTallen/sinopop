18ce4c80b8b8a42785a7e23a9f7dd38ce71bde25
Servus Z <allenalle0527@gmail.com>
2026-09-08 12:41:52 +0200

    prototype

---
+++ src/main/java/com/example/sinapop/SinapopApplication.java
@@ -0,0 +1,13 @@
+package com.example.sinapop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinapopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinapopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sinapop/SinapopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sinapop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinapopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
